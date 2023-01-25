6c4b7002059bbced31fbd7fc178051beb7f34542
ffouquet42 <ffouquet42@gmail.com>
2023-01-25 15:16:52 +0100

    create dev and includes directory

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-SRCS		=	main.c									\
+SRCS		=	dev/test.c									\
 
 OBJS		=	$(SRCS:.c=.o)
 
--- dev/test.c
+++ dev/test.c
@@ -1,23 +1,58 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   test.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fllanet <fllanet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/25 13:42:52 by fllanet           #+#    #+#             */
-/*   Updated: 2023/01/25 14:38:27 by fllanet          ###   ########.fr       */
+/*   Updated: 2023/01/25 15:15:41 by fllanet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <mlx.h>
+#include "../includes/so_long.h"
 
-int main()
+void ft_putchar(char c)
+{
+    write(1, &c, 1);
+}
+
+void	ft_putnbr(int n)
+{
+	long	nb;
+
+	nb = n;
+	if (nb < 0)
+	{
+		nb *= -1;
+		ft_putchar('-');
+	}
+	if (nb > 9)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+	else
+		ft_putchar(nb + 48);
+}
+
+int deal_key(int key, void *param)
+{
+    // ft_putchar('x');
+    ft_putnbr(key);
+    return (0);
+}
+
+int main(int argc, char **argv)
 {
     void *mlx_ptr;
     void *win_ptr;
 
     mlx_ptr = mlx_init();
-    win_ptr = mlx_new_window(mlx_ptr, 500, 500, "mlx");
+    win_ptr = mlx_new_window(mlx_ptr, 500, 500, "so_long");
+    mlx_pixel_put(mlx_ptr, win_ptr, 250, 250, 0xFFFFFF);
+    mlx_key_hook(win_ptr, deal_key, (void *)0);
+
     mlx_loop(mlx_ptr);
+    return (0);
 }
